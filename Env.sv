41e358a496cb8d093ca321884b44a99958856bb2
Maniyar Mohammed Saqlain <85744349+maniyarmohammedsaqlain@users.noreply.github.com>
2025-01-02 18:58:52 +0530

    Create Env.sv

---
+++ Env.sv
@@ -0,0 +1,19 @@
+class env extends uvm_env;
+  `uvm_component_utils(env);
+  agent a;
+  scoreboard scb;
+  function new(string path="env",uvm_component parent=null);
+    super.new(path,parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    a=agent::type_id::create("a",this);
+    scb=scoreboard::type_id::create("scb",this);
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    a.mon.send.connect(scb.recv);
+  endfunction
+endclass
